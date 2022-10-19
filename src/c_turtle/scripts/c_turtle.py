0bacbddbba06ef3fdb1d28412297340079d9742a
Ana Barros <up201806593@fe.up.pt>
2022-10-19 23:21:08 +0100

    Fix  turn

--- src/c_turtle/scripts/c_turtle.py
+++ src/c_turtle/scripts/c_turtle.py
@@ -39,6 +39,13 @@ class CTurtle:
         rospy.Subscriber("/scan", LaserScan, self.scanCallback)
 
     def scanCallback(self, scan):
+        lasers = [0] * len(scan.ranges)
+        angle = scan.angle_min
+        for i in range(len(scan.ranges)):
+            dist = scan.ranges[i]
+            lasers[i] = (angle, dist if not isnan(dist) else inf)
+            angle += scan.angle_increment
+
         dirs = {
             "front": {"dist": inf, "ang": 0},
             "front_left": {"dist": inf, "ang": rad225 * 2},
@@ -48,98 +55,80 @@ class CTurtle:
             "back_left": {"dist": inf, "ang": rad225 * 6},
             "back_right": {"dist": inf, "ang": -rad225 * 6},
             "back": {"dist": inf, "ang": pi},
+            "edges": {
+                "front": inf,
+                "left": inf,
+                "front_left": inf,
+                "back_left": inf,
+                "right": inf,
+                "front_right": inf,
+                "back_right": inf,
+            }
         }
 
-        curveMinIdx = 0
-        curveMaxIdx = 0
-        inCurve = False
         minDir = "front"
         minDist = inf
-        angle = scan.angle_min
-        for i in range(len(scan.ranges)):
-            dist = scan.ranges[i]
-            if not isnan(dist):
-                absAngle = abs(angle)
-                if absAngle <= rad225:
-                    key = "front"
-                elif absAngle <= rad225 * 3:
-                    key = "front_left" if angle > 0 else "front_right"
-                elif absAngle <= rad225 * 5:
-                    key = "left" if angle > 0 else "right"
-                elif absAngle <= rad225 * 7:
-                    key = "back_left" if angle > 0 else "back_right"
-                else:
-                    key = "back"
+        for laser in lasers:
+            angle = laser[0]
+            dist = laser[1]
 
-                if dist < dirs[key]["dist"]:
-                    # update sector
-                    dirs[key]["dist"] = dist
-                    dirs[key]["ang"] = angle
-                    if dist < minDist:
-                        # update global info
-                        minDir = key
-                        minDist = dist
-
-                        if not inCurve:
-                            inCurve = True
-                            curveMinIdx = curveMaxIdx = i
-                if inCurve:
-                    curveMaxIdx = i
+            absAngle = abs(angle)
+            if absAngle <= rad225:
+                key = "front"
+            elif absAngle <= rad225 * 3:
+                key = "front_left" if angle > 0 else "front_right"
+            elif absAngle <= rad225 * 5:
+                key = "left" if angle > 0 else "right"
+            elif absAngle <= rad225 * 7:
+                key = "back_left" if angle > 0 else "back_right"
             else:
-                inCurve = False
+                key = "back"
 
-            # loop increment
-            angle += scan.angle_increment
+            if angle < scan.angle_increment * 2:
+                dirs["edges"]["front"] = min(dirs["edges"]["front"], dist)
+            elif abs(angle - rad225 * 2) < scan.angle_increment * 2:
+                dirs["edges"]["front_left"] = min(dirs["edges"]["front_left"], dist)
+            elif abs(angle - -rad225 * 2) < scan.angle_increment * 2:
+                dirs["edges"]["front_right"] = min(dirs["edges"]["front_right"], dist)
+            elif abs(angle - rad225 * 4) < scan.angle_increment * 2:
+                dirs["edges"]["left"] = min(dirs["edges"]["left"], dist)
+            elif abs(angle - -rad225 * 4) < scan.angle_increment * 2:
+                dirs["edges"]["right"] = min(dirs["edges"]["right"], dist)
+            elif abs(angle - rad225 * 6) < scan.angle_increment * 2:
+                dirs["edges"]["back_left"] = min(dirs["edges"]["back_left"], dist)
+            elif abs(angle - -rad225 * 6) < scan.angle_increment * 2:
+                dirs["edges"]["back_right"] = min(dirs["edges"]["back_right"], dist)
 
-        if minDist == inf:
-            # no curve
-            curvature = 0
-        else:
-            curve = []
-            for i in range(curveMinIdx, curveMaxIdx + 1):
-                curve.append(
-                    (scan.ranges[i], scan.angle_min + i * scan.angle_increment)
-                )
-            curvature = self._calculateCurvature(curve)
+            if dist < dirs[key]["dist"]:
+                # update sector
+                dirs[key]["dist"] = dist
+                dirs[key]["ang"] = angle
+                if dist < minDist:
+                    # update global info
+                    minDir = key
+                    minDist = dist
 
         self.reactToScan(dirs, minDir)
 
-    def _calculateCurvature(self, curve):
-        curvature = 0
-
-        n = len(curve)
-        if n <= 3:
-            return curvature
-
-        a = int(n * 0.3)
-        c = n // 2
-        pc = curve[c]
-        pcX = pc[0] * cos(pc[1])
-        pcY = pc[0] * sin(pc[1])
-        for i in range(a, c):
-            pb = curve[c - i]
-            pbX = pb[0] * cos(pb[1])
-            pbY = pb[0] * sin(pb[1])
-            pf = curve[c + i]
-            pfX = pf[0] * cos(pf[1])
-            pfY = pf[0] * sin(pf[1])
-            curvature += (pfY - pcY) / (pfX - pcX)
-            curvature -= (pcY - pbY) / (pcX - pbX)
-
-        return 2 / n * curvature
-
     def reactToScan(self, dirs, minDir):
-        minDist = dirs[minDir]["dist"]
-        minAng = dirs[minDir]["ang"]
-
         # reset v
         self.linVel = 0
         self.angVel = 0
+
+        minDist = dirs[minDir]["dist"]
+        minAng = dirs[minDir]["ang"]
 
         if minDist == inf:
             # can't see anything => random walk
             self.wiggle()
             return
+
+        if dirs["edges"]["left"] == inf and dirs["edges"]["back_left"] == inf and dirs["edges"]["front"] == inf and dirs["edges"]["front_left"] != inf:
+            rospy.loginfo(dirs["edges"])
+            rospy.logerr("Foi pela esquierda")
+        #elif dirs["edges"]["right"] != inf and dirs["edges"]["front_right"] == inf and dirs["edges"]["back_right"] == inf:
+        #    rospy.loginfo(dirs["edges"])
+        #    rospy.logerr("Foi pela direita")
 
         if minDir.endswith("right"):
             front = min(dirs["front"]["dist"], dirs["front_left"]["dist"])
@@ -152,13 +141,25 @@ class CTurtle:
                 dirs["front_left"]["dist"],
             )
         self.linVel = CTurtle.maxLinVel * front / laserRange
-        self.linVel = 1
 
-        print("Angle: ", degrees(minAng), cos(minAng))
-        if minDir.endswith("left") or minDir == "front":
+        if minDir == "front":
+            minLeft = min(dirs["front_left"]["dist"], dirs["left"]["dist"])
+            minRight = min(dirs["front_right"]["dist"], dirs["right"]["dist"])
+            if minLeft < minRight:
+                turnDir = "left"
+            else:
+                turnDir = "right"
+        elif minDir.endswith("left"):
+            turnDir = "left"
+        else:
+            turnDir = "right"
+
+
+        if turnDir == "left":
             angDistTerm = cos(minAng) + (CTurtle.minDistFromWall - minDist)
         else:
-            angDistTerm = cos(pi - minAng) + (minDist - CTurtle.minDistFromWall)
+            angDistTerm = cos(pi - minAng) + \
+                (minDist - CTurtle.minDistFromWall)
         self.angVel = -CTurtle.k * self.linVel * angDistTerm
 
         self.moveTurtle()
@@ -178,9 +179,6 @@ class CTurtle:
     @linVel.setter
     def linVel(self, newLinVel):
         desiredVel = clamp(newLinVel, -CTurtle.maxLinVel, CTurtle.maxLinVel)
-        # TODO
-        self.vel.linear.x = desiredVel
-        return
 
         # v = v0 + a * t
         # desiredVel = self.linVel + a * (1/laserFreq)
@@ -247,7 +245,8 @@ class CTurtle:
         self.pub.publish(vel)
 
         moveService = rospy.ServiceProxy("/move_model", MoveModel)
-        moveService("CTurtle", Pose2D(uniform(-9, 8), uniform(-4, 14), uniform(0, 359)))
+        moveService("CTurtle", Pose2D(uniform(-9, 8),
+                    uniform(-4, 14), uniform(0, 359)))
 
 
 if __name__ == "__main__":
