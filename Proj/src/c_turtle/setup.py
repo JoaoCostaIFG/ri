ea0797dc14d1e5bc6c6e75e9fe0ab624e9aaef7a
TheZambi <gamingwithapotato@gmail.com>
2022-12-15 22:51:27 +0000

    Fix warning in setup.py

--- Proj/src/c_turtle/setup.py
+++ Proj/src/c_turtle/setup.py
@@ -12,6 +12,8 @@ setup(
         ('share/' + package_name + "/launch/", glob("launch/*launch*")),
         ('share/' + package_name + "/rviz/", glob("rviz/*")),
         ('share/' + package_name + "/c_world/" , glob('c_world/*')),
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
